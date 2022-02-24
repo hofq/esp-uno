00cc61d3185be2677f844d00d1f92af3e6c69625
hofq <>
2022-02-24 09:08:28 +0100

    added Traffic Light

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,34 @@
-# define LED 4
+#define R 25
+#define G 33
+#define B 32
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
-  pinMode(LED, OUTPUT);
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
+
+
+  digitalWrite(R, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(B, HIGH);
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
+
+
+  digitalWrite(G, LOW);
   delay(1000);
-  digitalWrite(LED, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(R, LOW);
   delay(1000); 
+  digitalWrite(G, HIGH);
+  digitalWrite(R, HIGH);
+  digitalWrite(R, LOW);
+  delay(1000);
+  digitalWrite(R, HIGH);
+  delay(1000);
+
 }
