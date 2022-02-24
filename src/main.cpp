ef821f4d92e1e78d25ec40ef1e282ede920f63bf
hofq <>
2022-02-24 10:53:32 +0100

    Created simple button reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
-#include <Arduino.h>
+#define btn 18
+int btnstate = 0;
+
+
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(btn, INPUT);
+
+
 }
 
 void loop() {
+  
+btnstate = digitalRead(btn);
+if (btnstate == 1) {
+  Serial.println("Button is Pressed.");
+}
+delay(300);
 
-  // put your main code here, to run repeatedly:
+
+
 }
